e6b777c04701116fdbaf5e5b0ba63e0f215fe087
Can Toraman <cantoraman@gmail.com>
2018-06-14 12:20:55 +0100

    initial commit

---
+++ models/student.rb
@@ -0,0 +1,39 @@
+require_relative('../db/sqlrunner.rb')
+
+
+class Student
+
+attr_reader :first_name, :last_name, :house, :age, :id
+
+
+def initialize (information)
+  @id = information['id'].to_i
+  @first_name = information['first_name']
+  @last_name = information['last_name']
+  @house = information['house']
+  @age = information['age'].to_i
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
