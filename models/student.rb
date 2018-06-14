d922d94921767bfe831a2886fe54598294b9973b
Can Toraman <cantoraman@gmail.com>
2018-06-14 12:41:01 +0100

    saves the students

--- models/student.rb
+++ models/student.rb
@@ -15,10 +15,27 @@ def initialize (information)
 end
 
 
+def name()
+  return "#{@first_name} #{@last_name}"
+end
 
-
-
-
+def save()
+  sql = "INSERT INTO students
+  (
+    first_name,
+    last_name,
+    house,
+    age
+  )
+  VALUES
+  (
+    $1, $2, $3, $4
+  )
+  RETURNING *"
+  values = [@first_name, @last_name, @house, @age]
+  student_data = SqlRunner.run(sql, values)
+  @id = student_data.first()['id'].to_i
+end
 
 
 
