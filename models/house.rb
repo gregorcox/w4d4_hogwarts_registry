121ba7a74ca3e422a861f30aeda6cd6e94b98369
Can Toraman <cantoraman@gmail.com>
2018-06-14 13:37:31 +0100

    house method adds

--- models/house.rb
+++ models/house.rb
@@ -29,44 +29,19 @@ def save()
   @id = house_data.first()['id'].to_i
 end
 
-def delete()
-  sql = "DELETE FROM students
-  WHERE id = $1"
-  values = [@id]
-  SqlRunner.run( sql, values )
-end
-
 def self.all()
-  sql = "SELECT * FROM students"
-  students = SqlRunner.run( sql )
-  result = students.map { |student| Student.new( student ) }
+  sql = "SELECT * FROM houses"
+  houses = SqlRunner.run( sql )
+  result = houses.map { |house| House.new( house ) }
   return result
 end
 
 def self.find( id )
-  sql = "SELECT * FROM students WHERE id = $1"
+  sql = "SELECT * FROM houses WHERE id = $1"
   values = [id]
-  student = SqlRunner.run( sql, values )
-  result = Student.new( student.first )
+  house = SqlRunner.run( sql, values )
+  result = House.new( house.first )
   return result
 end
-
-def self.delete_all()
-  sql = "DELETE FROM students;"
-  SqlRunner.run(sql)
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 end
